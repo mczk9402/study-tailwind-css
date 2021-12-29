84c7b343a1c7e9d907ce6cc7a14037ba5115464a
望月祐汰 <mczk9402@gmail.com>
2021-12-30 03:18:18 +0900

    いろいろ検証

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  mode: 'jit',
+  content: ['index.html'],
+  theme: {
+    extend: {
+      fontSize: {
+        '10xl': '32px',
+      },
+      fontFamily: {
+        fantasy: 'fantasy',
+      },
+    },
+  },
+  plugins: [],
+};
